d9bc1dedcdd7571b052e3d1c370924c2789ef163
Zidan Raka <zidanraka99@gmail.com>
2023-07-15 04:02:06 +0700

    Add How to Play, About and Setting

--- js/game.js
+++ js/game.js
@@ -9,7 +9,11 @@ var gambar = {
   maxBtn: "maxBtn.png",
   minBtn: "minBtn.png",
   help: "Help.png",
+  aboutBtn: "About.png",
   back: "btnBack.png",
+  home: "Setting.png",
+  exit: "Exit.png",
+  resume: "Resume.png",
   idle: "Idle.png",
   run: "Run.png",
   jump: "Jump.png",
@@ -39,11 +43,13 @@ function startScreen() {
     jalankan(halamanCover);
   }
 }
+
 function halamanCover() {
   hapusLayar("#99627a");
   gambarFull(dataGambar.cover);
   var playBtn = tombol(dataGambar.playBtn, 1100, 500);
-  var howtoPlay = tombol(dataGambar.help, 100, 500);
+  var howtoPlay = tombol(dataGambar.help, 100, 550);
+  var about = tombol(dataGambar.aboutBtn, 50, 50);
   if (tekan(playBtn) || game.spasi) {
     if (game.aktif) {
       //mulai game dengan menambahkan transisi
@@ -54,10 +60,15 @@ function halamanCover() {
       transisi("out", setAwal);
     }
   }
-  // Jika tombol howtoplay ditekan transisi ke halaman (How to Play)
+  // Tombol (How to Play)
   if (tekan(howtoPlay)) {
     game.warnaTransisi = "#116d6e";
     transisi("out", setHow);
+  }
+  // Tombol (About)
+  if (tekan(about)) {
+    game.warnaTransisi = "#116d6e";
+    transisi("out", setAbout);
   }
   resizeBtn(1150, 50);
   efekTransisi();
@@ -69,6 +80,12 @@ function setHow() {
   jalankan(HowtoPlay);
 }
 
+// Transisi ke halaman (About)
+function setAbout() {
+  transisi("in");
+  jalankan(about);
+}
+
 // Transisi kembali ke halaman cover
 function backHome() {
   transisi("in");
@@ -78,12 +95,76 @@ function backHome() {
 // Halaman (How to play)
 function HowtoPlay() {
   hapusLayar("#99627a");
-  teks("How to Play", 450, 100, "Cursive-bold-30pt-left-black");
-  teks("Right Arrow : Go to Right", 350, 230, "Cursive-bold-30pt-left-black");
-  teks("Left Arrow : Go to Left", 350, 330, "Cursive-bold-30pt-left-black");
-  teks("Up Arrow : Jump", 350, 430, "Cursive-bold-30pt-left-black");
-  teks("Enjoy Playing 👾", 410, 540, "Cursive-bold-30pt-left-black");
-  var btnBack = tombol(dataGambar.back, 100, 100);
+  teks("How to Play", 320, 100, "Cursive-bold-30pt-left-black");
+  teks("Right Arrow : Go to Right", 270, 180, "Cursive-bold-20pt-left-black");
+  teks("Left Arrow : Go to Left", 270, 230, "Cursive-bold-20pt-left-black");
+  teks("Up Arrow : Jump", 270, 280, "Cursive-bold-20pt-left-black");
+  teks("Cara Bermain", 720, 100, "Cursive-bold-30pt-left-black");
+  teks(
+    "Arah Kanan : Bergerak ke Kanan",
+    670,
+    180,
+    "Cursive-bold-20pt-left-black"
+  );
+  teks(
+    "Arah Kiri : Bergerak ke Kiri",
+    670,
+    230,
+    "Cursive-bold-20pt-left-black"
+  );
+  teks("Arah Atas : Lompat", 670, 280, "Cursive-bold-20pt-left-black");
+  teks("Enjoy Playing 👾", 450, 500, "Cursive-bold-32pt-left-black");
+  var btnBack = tombol(dataGambar.back, 80, 80);
+  if (tekan(btnBack)) {
+    game.warnaTransisi = "#116d6e";
+    transisi("out", backHome);
+  }
+  efekTransisi();
+}
+
+// Halaman About
+function about() {
+  hapusLayar("#99627a");
+  teks("About", 550, 100, "Cursive-bold-30pt-left-black");
+  teks(
+    "This game tell us about a guy named 'Craig', who has to get out of a place full of traps and various kind",
+    100,
+    180,
+    "Cursive-bold-14pt-left-black"
+  );
+  teks(
+    "of monsters. Craig is just and ordinary guy, but his always wear a pink clothes and his eyes look like crazy",
+    100,
+    205,
+    "Cursive-bold-14pt-left-black"
+  );
+  teks(
+    "people. Because of that his known as Crazy Pink Guy, or as we call it 'Craig'.",
+    100,
+    230,
+    "Cursive-bold-14pt-left-black"
+  );
+  teks("Tentang", 520, 350, "Cursive-bold-30pt-left-black");
+  teks(
+    "Game ini bercerita tentang seseorang yang bernama 'Craig', yang harus keluar dari tempat yang penuh dengan",
+    100,
+    430,
+    "Cursive-bold-14pt-left-black"
+  );
+  teks(
+    "jebakan dan berbagai jenis monster. Craig hanyalah seseorang yang biasa saja, tapi dia selalu memakai pakaian",
+    100,
+    455,
+    "Cursive-bold-14pt-left-black"
+  );
+  teks(
+    "pink dan matanya terlihat seperti orang gila. Karena itu dia dikenal sebagai Crazy Pink Guy, atau kita menyebutnya",
+    100,
+    480,
+    "Cursive-bold-14pt-left-black"
+  );
+  teks("'Craig'.", 100, 505, "Cursive-bold-14pt-left-black");
+  var btnBack = tombol(dataGambar.back, 80, 80);
   if (tekan(btnBack)) {
     game.warnaTransisi = "#116d6e";
     transisi("out", backHome);
@@ -100,22 +181,18 @@ function setAwal() {
   game.hero.animJatuh = dataGambar.fall;
   game.hero.animMati = dataGambar.hit;
   game.skalaSprite = 2;
-
   //setPlatform(map_1, dataGambar.tileset, 32, game.hero);
   setPlatform(this["map_" + game.level], dataGambar.tileset, 32, game.hero);
   game.gameOver = ulangiPermainan;
-
   //set item
   setPlatformItem(1, dataGambar.item1);
   setPlatformItem(2, dataGambar.item2);
-
   //set enemy
   var musuh1 = {};
   musuh1.animDiam = dataGambar.musuh1Idle;
   musuh1.animJalan = dataGambar.musuh1Run;
   musuh1.animMati = dataGambar.musuh1Hit;
   setPlatformEnemy(1, musuh1);
-
   //set trigger
   setPlatformTrigger(1, dataGambar.bendera);
   if (game.status == "mulai") {
@@ -132,8 +209,8 @@ function mulaiPermainan() {
 function ulangiPermainan() {
   setAwal();
   game.aktif = true;
-  jalankan(gameLoop);
   game.score = 0;
+  jalankan(gameLoop);
 }
 
 function gameLoop() {
@@ -150,8 +227,42 @@ function gameLoop() {
   latar(dataGambar.bg, 0, 0.5);
   buatLevel();
   cekItem();
-  teks(game.score, 550, 60, "Cursive-bold-30pt-left-ungu");
-  teks("Level " + game.level, 50, 60, "Cursive-bold-30pt-left-ungu");
+  teks(game.score, 550, 60, "Monaco-bold-30pt-left-ungu");
+  teks("Level " + game.level, 50, 60, "Monaco-bold-30pt-left-ungu");
+  // Tombol setting dalam game
+  const home = tombol(dataGambar.home, 1150, 50);
+  if (tekan(home)) {
+    game.warnaTransisi = "#116d6e";
+    transisi("out", setHome);
+  }
+  efekTransisi();
+}
+
+// Transisi ke halaman Setting
+function setHome() {
+  transisi("in");
+  jalankan(pilihanHome);
+}
+
+// Halaman Setting
+function pilihanHome() {
+  hapusLayar("#99627a");
+  teks("Do you want to Exit?", 400, 100, "Cursive-bold-30pt-left-black");
+  var btnBack = tombol(dataGambar.back, 80, 80);
+  var exit = tombol(dataGambar.exit, 470, 250);
+  var resume = tombol(dataGambar.resume, 750, 250);
+  if (tekan(btnBack)) {
+    game.warnaTransisi = "#116d6e";
+    transisi("out", mulaiPermainan);
+  }
+  if (tekan(exit)) {
+    game.warnaTransisi = "#116d6e";
+    transisi("out", backHome);
+  }
+  if (tekan(resume)) {
+    game.warnaTransisi = "#116d6e";
+    transisi("out", mulaiPermainan);
+  }
   efekTransisi();
 }
 
